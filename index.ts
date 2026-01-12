ff8408387c772fe98950f647eeb757aa72dfe8f4
Brandon Lee <brandonmlee85@gmail.com>
2026-01-12 18:12:11 -0500

    Add generated prisma schema

---
+++ index.ts
@@ -0,0 +1,17 @@
+import 'dotenv/config';
+import { PrismaClient as _PrismaClient, Prisma } from './generated/prisma-client';
+import { PrismaPg } from '@prisma/adapter-pg'
+
+const {
+  DATABASE_URL = 'postgresql://postgres:postgres@localhost:5432/band-together'
+} = process.env;
+
+// Change the adapter depending on the db used - in this case, PostgreSQL
+const adapter = new PrismaPg({
+  connectionString: DATABASE_URL
+});
+
+export const PrismaClient = (options?: Prisma.PrismaClientOptions) => new _PrismaClient({
+  adapter,
+  ...options
+});
